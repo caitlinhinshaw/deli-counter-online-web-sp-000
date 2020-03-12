4d5e57a04499edd8da7a8b7d0ba1503ae5894bfd
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-12 01:56:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(katz_deli)
-  if katz_deli.length < 0
+  if katz_deli.length > 0
     line = "The line is currently: "
     katz_deli.each_with_index do |person, index|
       line << "#{index+1}. #{Person} "
